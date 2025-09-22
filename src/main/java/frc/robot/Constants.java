e2999fd985140f73c35175b85f2a42b535c194bb
znotfireman <znotfireman@gmail.com>
2025-09-22 18:15:31 +0700

    more progress

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,5 @@
+package frc.robot;
+
+public final class Constants {
+    public static final double kPTurning = 0.5;
+}
--- src/main/java/frc/robot/drive/SwerveModule.java
+++ src/main/java/frc/robot/drive/SwerveModule.java
@@ -1,6 +1,46 @@
 package frc.robot.drive;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.AnalogInput;
+import frc.robot.Constants;
+
 public class SwerveModule {
-    // I NEED TO DO MUN
-    // HAI WHAT DID YOU DO TO THE DOC
+    private final TalonSRX driveMotor;
+    private final TalonSRX turningMotor;
+
+    private final PIDController turningPidController;
+
+    private final AnalogInput absoluteEncoder;
+    private double absoluteEncoderOffsetRad = 0;
+    private boolean absoluteEncoderInverted = false;
+
+    public SwerveModule(TalonSRX driveMotor, TalonSRX turningMotor, AnalogInput absoluteEncoder) {
+        this.driveMotor = driveMotor;
+        this.turningMotor = turningMotor;
+        this.absoluteEncoder = absoluteEncoder;
+
+        this.turningPidController = new PIDController(Constants.kPTurning, 0, 0);
+    }
+
+    public SwerveModule setAbsoluteEncoderOffsetRad(double offsetRad) {
+        this.absoluteEncoderOffsetRad = offsetRad;
+        return this;
+    }
+
+    public SwerveModule setAbsoluteEncoderInverted(boolean isInverted) {
+        this.absoluteEncoderInverted = isInverted;
+        return this;
+    }
+
+    public SwerveModule setDriveMotorInverted(boolean isInverted) {
+        this.driveMotor.setInverted(isInverted);
+        return this;
+    }
+
+    public SwerveModule setTurningMotorInverted(boolean isInverted) {
+        this.turningMotor.setInverted(isInverted);
+        return this;
+    }
 }
